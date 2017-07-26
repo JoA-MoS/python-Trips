869a7c1ccc4c54a20b5441cbbdf54408eedde36e
Justin Dietz <justin.r.dietz@gmail.com>
2017-07-26 13:08:53 -0700

    Validations

--- apps/trips/validate.py
+++ apps/trips/validate.py
@@ -1,4 +1,8 @@
+from django.utils import timezone, dateparse
 from ..users.error import Error
+from dateutil import parser
+from datetime import datetime
+
 
 def trip_form(data):
     errors = []
@@ -6,18 +10,19 @@ def trip_form(data):
         valid, msg = destination(data['destination'])
         if not valid:
             errors.append(Error('destination', msg))
-        # valid, msg = description(data['description'])
-        # if not valid:
-        #     errors.append(Error('description', msg))
-        valid, msg = valid_date(data['start_date'])
+        valid, msg = description(data['description'])
         if not valid:
-            errors.append(Error('start_date', msg))
-        valid, msg = valid_date(data['end_date'])
-        if not valid:
-            errors.append(Error('end_date', msg))
-        valid, msg = start_less_than_end(data['start_date'], data['end_date'])
-        if not valid:
-            errors.append(Error('start_date, end_date', msg))
+            errors.append(Error('description', msg))
+        start_valid, msg = valid_date(data['start_date'])
+        if not start_valid:
+            errors.append(Error('start_date', 'Start date - ' + msg))
+        end_valid, msg = valid_date(data['end_date'])
+        if not end_valid:
+            errors.append(Error('end_date', 'End date - ' + msg))
+        if start_valid and end_valid:
+            valid, msg = start_less_than_end(data['start_date'], data['end_date'])
+            if not valid:
+                errors.append(Error('start_date, end_date', msg))
         # if all the fields look good make sure the user doesnt already exist
     else:
         errors.append(
@@ -33,18 +38,34 @@ def destination(val):
     else:
         return False, 'destination must be at least 2 characters'
 
-def valid_date(val):
-    if val:
-        #TODO validate it is actually a date
+def description(val):
+    if len(val) > 0:
         return True, None
     else:
+        return False, 'description must be entered'
+
+def valid_date(val):
+    try:
+        print val
+        date = dateparse.parse_date(val)        
+        if timezone.localdate() <= date:
+            print 'valid date'
+            return True, None
+        else:
+            return False, 'date must be in the future'
+    except:
         return False, 'Please specify a date in MM/DD/YYYY format'
 
-def start_less_than_end(val1, val2):
-    if val1<=val2:
-        #TODO validate it is actually a date
-        return True, None
-    else:
-        return False, 'Please specify a start date before the end date'
 
+def start_less_than_end(val1, val2):
+    try:
+        date1 = dateparse.parse_date(val1)
+        date2 = dateparse.parse_date(val2)
+        if date1 < date2:
+            return True, None
+        else:
+            return False, 'Please specify a start date before the end date'
+    except:
+        return False, 'Please specify a date in MM/DD/YYYY format'
+  
 
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,13 +4,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^users/$', views.index),
+    url(r'^users/$', views.index), #this is not need for the belt
     url(r'^login/$', views.show_login, name='disp_login'),
     url(r'^login/process/$', views.login, name='do_login'),
     url(r'^logout/$', views.logout, name='do_logout'),
     url(r'^combined/', views.show_register_login, name='disp_reg_login'),
     url(r'^register/', views.show_register, name='disp_reg'),
     url(r'^users/create/', views.create, name='create'),
+    # Everyont after here is not needed for the belt
     url(r'^users/list/$', views.show_users_list, name='disp_users'),
     url(r'^users/(?P<user_id>\d+)/$', views.show, name='show'),
     url(r'^users/(?P<user_id>\d+)/edit/', views.edit, name='edit'),
